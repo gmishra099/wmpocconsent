7a346b4773468dcea8461849dfc890e667523908
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-22 15:47:55 +0530

    Update Consent.java

--- src/main/java/com/wm/model/Consent.java
+++ src/main/java/com/wm/model/Consent.java
@@ -15,89 +15,42 @@ public class Consent {
     private Long id;
     
     @Column(name = "sfid")
-    private String conSFDC;
+    private String consentSFDC;
     
-    @Column(name = "ContactPointId")
-    private String contactPoint;
-    
-    @Column(name = "Consent__c")
-    private String consentState;
-    
-    @Column(name = "Name")
-    private String consentName;
-    
-    @Column(name = "ConsentCapturedSource")
-    private String consentCapturedSource;
-    
-    @Column(name = "Comm_Sub__c")
-    private String commSubscription;
-    
+  
     @Column(name = "ConsentGiverId")
     private String consentGiverId;
+
 
 	public Long getId() {
 		return id;
 	}
 
+
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-	public String getConSFDC() {
-		return conSFDC;
+
+	public String getConsentSFDC() {
+		return consentSFDC;
 	}
 
-	public void setConSFDC(String conSFDC) {
-		this.conSFDC = conSFDC;
+
+	public void setConsentSFDC(String consentSFDC) {
+		this.consentSFDC = consentSFDC;
 	}
 
-	public String getContactPoint() {
-		return contactPoint;
-	}
-
-	public void setContactPoint(String contactPoint) {
-		this.contactPoint = contactPoint;
-	}
-
-	public String getConsentState() {
-		return consentState;
-	}
-
-	public void setConsentState(String consentState) {
-		this.consentState = consentState;
-	}
-
-	public String getConsentName() {
-		return consentName;
-	}
-
-	public void setConsentName(String consentName) {
-		this.consentName = consentName;
-	}
-
-	public String getConsentCapturedSource() {
-		return consentCapturedSource;
-	}
-
-	public void setConsentCapturedSource(String consentCapturedSource) {
-		this.consentCapturedSource = consentCapturedSource;
-	}
-
-	public String getCommSubscription() {
-		return commSubscription;
-	}
-
-	public void setCommSubscription(String commSubscription) {
-		this.commSubscription = commSubscription;
-	}
 
 	public String getConsentGiverId() {
 		return consentGiverId;
 	}
 
+
 	public void setConsentGiverId(String consentGiverId) {
 		this.consentGiverId = consentGiverId;
 	}
+
 
     
     
