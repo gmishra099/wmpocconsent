df714fa51704f5015dfe1fd69e1f1154493a2ae5
gmishra099 <58309751+gmishra099@users.noreply.github.com>
2020-09-22 16:12:55 +0530

    Update Consent.java

--- src/main/java/com/wm/model/Consent.java
+++ src/main/java/com/wm/model/Consent.java
@@ -20,36 +20,89 @@ public class Consent {
   
     @Column(name = "consentgiverid")
     private String consentGiverId;
-
+    
+    @Column(name = "Consent__c")
+    private String Consent__c;
+    
+    @Column(name = "Name")
+    private String name;
+    
+    @Column(name = "ContactPointId")
+    private String contactPointId;
+    
+    @Column(name = "Comm_sub__c")
+    private String comm_sub__c;
+    
+    @Column(name = "Commsubscriptionchanneltypeid")
+    private String commSubscriptionChannelTypeId;
 
 	public Long getId() {
 		return id;
 	}
 
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 
 	public String getConsentSFDC() {
 		return consentSFDC;
 	}
 
-
 	public void setConsentSFDC(String consentSFDC) {
 		this.consentSFDC = consentSFDC;
 	}
-
 
 	public String getConsentGiverId() {
 		return consentGiverId;
 	}
 
-
 	public void setConsentGiverId(String consentGiverId) {
 		this.consentGiverId = consentGiverId;
 	}
+
+	public String getConsent__c() {
+		return Consent__c;
+	}
+
+	public void setConsent__c(String consent__c) {
+		Consent__c = consent__c;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContactPointId() {
+		return contactPointId;
+	}
+
+	public void setContactPointId(String contactPointId) {
+		this.contactPointId = contactPointId;
+	}
+
+	public String getComm_sub__c() {
+		return comm_sub__c;
+	}
+
+	public void setComm_sub__c(String comm_sub__c) {
+		this.comm_sub__c = comm_sub__c;
+	}
+
+	public String getCommSubscriptionChannelTypeId() {
+		return commSubscriptionChannelTypeId;
+	}
+
+	public void setCommSubscriptionChannelTypeId(String commSubscriptionChannelTypeId) {
+		this.commSubscriptionChannelTypeId = commSubscriptionChannelTypeId;
+	}
+
+    
+
+
 
 
 
